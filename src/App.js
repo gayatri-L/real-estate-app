3944b8f3cfa71922893de9c225fe6a2e5e7a2a2a
Aishwarya Annam <annamaishwarya@gmail.com>
2025-02-19 21:50:48 +0530

    Adding updated package&App file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
-import React, { useState } from "react";
-import Header from './Components/Header';
-import Footer from './Components/Footer';
 import Home from "./Pages/Home";
-import Display_D from "./Pages/Display_D";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import YourPreferences from "./Components/YourPreferences";
 import Display from "./Pages/Display_D";
 
 const App = () => {
-
-  const userData = { bedrooms: "3", budget: "50L", location: "Mumbai" }; // Example data
 
   return (
     <Router> {/* ✅ Wrap everything inside Router */}
