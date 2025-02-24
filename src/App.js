4671a837252dca30b6abf880f17198b57b534694
Ashwini Vangari <ashwinivangari2610@gmail.com>
2025-02-24 13:28:45 +0530

    Updated UI and API calls for the images

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import React, { useState } from "react";
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import Home from "./Pages/Home";
-import Display_D from "./Pages/Display_D";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import YourPreferences from "./Components/YourPreferences";
-import Display from "./Pages/Display_D";
+import Home from "./Pages/Home";
+import React from "react";
+import DisplayImages from "./Components/DisplayImages";
+import MultipleImageUpload from "./Components/MultipleImageUpload";
+import SingleImageDisplay from "./Components/SingleImageDisplay";
+import SingleImageUpload from "./Components/SingleImageUpload";
 
 const App = () => {
-
-  const userData = { bedrooms: "3", budget: "50L", location: "Mumbai" }; // Example data
-
   return (
-    <Router> {/* ✅ Wrap everything inside Router */}
+    <Router>
       <Routes>
-        <Route path="/" element={<Home />}/>
-        {/* <Route path="/" element={<YourPreferences data={userData} />} /> */}
-        <Route path="/Display_D" element={<Display />} />
-      </Routes>
+       <Route path="/" element={<Home />}></Route>
+       <Route path="/display" element={<DisplayImages />}></Route>
+       <Route path="/multiple-upload" element={<MultipleImageUpload />}></Route>
+       <Route path="/single-upload" element={<SingleImageUpload />}></Route>
+       <Route path="/single-display" element={<SingleImageDisplay />}></Route>
+       </Routes>
     </Router>
-    
   );
 };
 
